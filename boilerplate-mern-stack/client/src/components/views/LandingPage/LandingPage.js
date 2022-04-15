76efc2ecee4116cf8e4af4831024516b00a4173f
곽우진 <woojin@gwag-ujin-ui-MacBookPro.local>
2022-04-15 21:57:26 +0900

    add filtering boxes

--- boilerplate-mern-stack/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack/client/src/components/views/LandingPage/LandingPage.js
@@ -3,22 +3,57 @@ import React, { useEffect, useState } from 'react'
 import { Icon, Col, Card, Row } from 'antd'
 import Meta from 'antd/lib/card/Meta';
 import ImageSlider from '../../utils/ImageSlider';
+import CheckBox from './Sections/CheckBox';
+import RadioBox from './Sections/RadioBox';
+import { kinds, price } from './Sections/Datas'
 
 function LandingPage() {
 
     const [products, setProducts] = useState([])
+    const [skip, setSkip] = useState(0)
+    const [limit, setLimit] = useState(4)
+    const [PostSize, setPostSize] = useState(0)
+    const [Filters, setFilters] = useState({
+        kinds: [],
+        price: []
+    })
 
-    useEffect(() => {
-        let body = 
-        Axios.post('/api/product/products', )
+    const getProducts = (body) => {
+        Axios.post('/api/product/products', body)
             .then(response => {
                 if (response.data.success) {
-                    setProducts(response.data.productInfo)
+                    if (body.loadMore) {
+                        setProducts([...products, ...response.data.productInfo])
+                    } else {
+                        setProducts(response.data.productInfo)
+                    }
+                    setPostSize(response.data.postSize)
                 } else {
                     alert("상품들을 가져오는데 실패했어요")
                 }
             })
-    })
+    }
+
+    useEffect(() => {
+        let body = {
+            skip: skip,
+            limit: limit
+        }
+        getProducts(body)
+    }, [])
+
+    const loadMoreHandler = () => {
+        let nextSkip = skip + limit
+        let Limit = limit + limit
+        let body = {
+            skip: nextSkip,
+            limit: Limit,
+            loadMore: true
+        }
+        getProducts(body)
+        setSkip(nextSkip)
+    }
+
     //                    <Card cover={<ImageSlider images={product.images}/>}>
     //cover={<a href={`/product/${product._id}`} ><ImageSlider images={product.images} /></a>
     const renderCards = products.map((product, index) => {
@@ -26,11 +61,46 @@ function LandingPage() {
                     <Card cover={<a href={`/product/${product._id}`} ><ImageSlider images={product.images} /></a>}>
                         <Meta 
                             title={product.title}
-                            description={`$ ${product.price}`}
+                            description={`$${product.price}`}
                         />
                     </Card>
                 </Col>
     })
+
+    const handlePrice = (value) => {
+        const data = price;
+        let array = [];
+        for (let key in data) {
+            if (data[key]._id === parseInt(value)) {
+                array = data[key].array
+            }
+        }
+        return array
+    }
+
+    const showFilteredResults = (filter) => {
+        let body = {
+            skip: 0,
+            limit: limit,
+            filters: filter
+        }
+        getProducts(body)
+        setSkip(0)
+    }
+
+    const handleFilters = (filter, category) => {
+        const newFilters = { ...Filters }
+        newFilters[category] = filter
+
+        if (category === "price") {
+            let priceValues = handlePrice(filter)
+            newFilters[category] = priceValues
+
+        }
+
+        showFilteredResults(newFilters)
+        setFilters(newFilters)
+    }
 
     return (
         <div style={{
@@ -39,17 +109,29 @@ function LandingPage() {
             <div style={{ textAlign: 'center' }}>
                 <h2>당신의 날개를 찾아 보세요 <Icon type='fire' /></h2><br/>
             </div>
+
+            <Row>
+                <Col lg={12} xs={24}>
+                    <CheckBox list={ kinds } handleFilters={ filters => handleFilters(filters, "kind")}/>
+                </Col>
+                <Col lg={12} xs={24}>
+                    <RadioBox list={ price } handleFilters={ filters => handleFilters(filters, "price")}/>
+                </Col>
+            </Row>
+
             <Row gutter={[16, 16]}>
             { renderCards }
             </Row>
             <br/>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button style={{ 
-                    backgroundColor: 'lavender', border: 'none', borderRadius: '15px', padding: '8px 16px'}}>
-                        더보기
-                </button>
-            </div>
-
+            { PostSize >= limit &&
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button style={{ 
+                        backgroundColor: 'lavender', border: 'none', borderRadius: '15px', padding: '8px 16px'}}
+                        onClick={ loadMoreHandler }>
+                            더보기
+                    </button>
+                </div>
+            }
         </div>
     )
 }
--- boilerplate-mern-stack/server/routes/product.js
+++ boilerplate-mern-stack/server/routes/product.js
@@ -34,11 +34,32 @@ router.post('/', (req, res) => {
 })
 
 router.post('/products', (req, res) => {
-    Product.find()
+    let limit = req.body.limit ? parseInt(req.body.limit) : 20
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0
+
+    let findArgs = {}
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                }
+
+            } else {
+                findArgs[key] = req.body.filters[key]
+            }
+        }
+    }
+
+    Product.find(findArgs)
         .populate("writer")
+        .skip(skip)
+        .limit(limit)
         .exec((err, productInfo) => {
             if (err) return res.status(400).json({ success: false, err })
-            return res.status(200).json({ success:true, productInfo })
+            return res.status(200).json({ success:true, productInfo, postSize: productInfo.length })
         })
 })
 
